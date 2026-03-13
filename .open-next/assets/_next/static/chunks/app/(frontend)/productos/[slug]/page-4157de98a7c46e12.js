e4ca3fe0fe020f74e161ce4fc133587bca7bd283
greenlantern02 <54874859+greenlantern02@users.noreply.github.com>
2026-03-12 20:35:34 -0600

    Delete .open-next directory

--- .open-next/assets/_next/static/chunks/app/(frontend)/productos/[slug]/page-4157de98a7c46e12.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[834],{1862:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,844,23)),Promise.resolve().then(n.t.bind(n,2890,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[509,80,102,358],()=>s(1862)),_N_E=e.O()}]);
--- .open-next/assets/_next/static/chunks/app/admin/(auth)/productos/nuevo/page-d057fe88c130b9cd.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[568],{},_=>{var e=e=>_(_.s=e);_.O(0,[509,637,80,102,358],()=>e(9637)),_N_E=_.O()}]);
